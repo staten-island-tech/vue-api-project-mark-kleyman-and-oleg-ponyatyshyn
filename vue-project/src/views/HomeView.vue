a500c54e5c7481ad710c2a196a25e6a176865c19
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-03-12 21:50:19 -0400

    ss

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,34 +1,69 @@
 <template>
-  <div>
-    <BarChart/>
-  </div>
+  <Bar
+    id="my-chart-id"
+    :options="state.chartOptions"
+    :data="state.chartData"
+  />
 </template>
 
 <script setup>
+import { ref, reactive, onBeforeMount } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
-import BarChart from '@/components/BarChart.vue'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-import {ref, onBeforeMount} from 'vue'
-const crashes = []
+
 async function getCrashes() {
-  let res = await fetch ("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
-  let data = await res.json();
-  data.forEach((item)=>{
-   crashes.push(item.contributing_factor_vehicle_1)
-   crashes.sort()
-  })
-  function onlyUnique(value, index, array) {
-    return array.indexOf(value) === index;
+  try {
+    const res = await fetch('https://data.cityofnewyork.us/resource/h9gi-nx95.json');
+    const data = await res.json();
+    console.log(data)
+    data.forEach((item) => {
+      crashes.value.push(item.contributing_factor_vehicle_1);
+    });
+    const crashCounts = data.reduce((acc, item) => {
+      const crash = item.contributing_factor_vehicle_1;
+      if (!acc[crash]) {
+        acc[crash] = 1;
+      } else {
+        acc[crash]++;
+      }
+      return acc;
+    }, {});
+
+    const counts = Object.values(crashCounts);
+    chartDataSelf.push(counts[0])
+    console.log(chartDataSelf)
+    function onlyUnique(value, index, array) {
+      return array.indexOf(value) === index;
+    }
+    
+    const unique = crashes.value.filter(onlyUnique);
+    
+    chartLabels.push(unique[0])
+
+
+  } catch (error) {
+    console.error(error); 
   }
-  const unique = crashes.filter(onlyUnique)
-  console.log(unique)
 }
+const crashes = ref([]);
+let chartLabels = []
+let chartDataSelf = []
+const state = reactive({
+  chartData: {
+    labels: [chartLabels, 'skibidi', 'toilet'],
+    datasets: [{ data: [chartDataSelf] }],
+  },
+  chartOptions: {
+    responsive: true,
 
-onBeforeMount(() => {
-  getCrashes()
+  },
 });
+onBeforeMount(() => {
+  getCrashes();
+  
+});
+
 </script>
-
-<style lang="scss" scoped>
-
-</style>
